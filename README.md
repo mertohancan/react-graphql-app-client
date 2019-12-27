9beeffc8f4671c66d5ec4a3735e977a8ee0d0015
mertohancan <ohancanmert@gmail.com>
2019-12-27 16:11:34 +0300

    Elint configuration

--- README.md
+++ README.md
@@ -6,8 +6,14 @@ In the project directory, you can run:
 
 ### `yarn start`
 
+<<<<<<< Updated upstream
 Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+=======
+2.  Install dependencies
+> cd react-graphql-app-client
+npm install or yarn install asdc
+>>>>>>> Stashed changes
 
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
--- src/util/hooks.js
+++ src/util/hooks.js
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+import { useState } from "react";
 
+// eslint-disable-next-line import/prefer-default-export
 export const useForm = (callback, initialState = {}) => {
   const [values, setValues] = useState(initialState);
 
-  const onChange = (event) => {
+  const onChange = event => {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
 
-  const onSubmit = (event) => {
+  const onSubmit = event => {
     event.preventDefault();
     callback();
   };
