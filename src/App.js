9fe1b3e0ee9915b5bffcd7043f15358039c35de2
mertohancan <ohancanmert@gmail.com>
2020-01-05 22:36:15 +0300

    transition to es6

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css';
-
-import { Container } from 'semantic-ui-react';
+import Router from './routes/Router';
 import { AuthProvider } from './context/auth';
-import Home from './screens/Home';
-import Login from './screens/Login';
-import Register from './screens/Register';
-import Header from './components/Header';
 
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <div className="ui container">
-          <Container>
-            <Header />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-          </Container>
-        </div>
-      </Router>
-    </AuthProvider>
-  );
-}
+const App = () => (
+  <AuthProvider>
+    <Router />
+  </AuthProvider>
+);
 
 export default App;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
 import { Grid } from 'semantic-ui-react';
+import FETCH_POSTS_QUERY from '../queries/fetch';
 
 import PostCard from '../components/PostCard';
 
-function Home() {
+const Home = () => {
   let posts = '';
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
 
@@ -30,28 +30,6 @@ function Home() {
       )}
     </Grid>
   );
-}
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
+};
 
 export default Home;
