0ad2ce686b8cd70bc92a52edcb578bc9e6d07d92
mertohancan <ohancanmert@gmail.com>
2020-01-08 11:35:12 +0300

    added hot loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,6 @@
-module.exports = function override(config) {
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+
+module.exports = function override(config, env) {
+  config = rewireReactHotLoader(config, env);
   return config;
 };
