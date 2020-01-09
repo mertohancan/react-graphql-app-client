1510dc24747fc00a0e30f33ab5a1dbb79192542a
mertohancan <ohancanmert@gmail.com>
2020-01-09 11:51:24 +0300

    Chore/hot loader (#19)
    * hot loader configuration
    
    * added hot loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,10 @@
-module.exports = function override(config) {
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+
+module.exports = function(config, env) {
+  config = rewireReactHotLoader(config, env);
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    'react-dom': '@hot-loader/react-dom',
+  };
   return config;
 };
